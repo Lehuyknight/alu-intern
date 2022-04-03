62bcbee27042949f96a6ed101594a53dacb92777
Lê Huy Knight <loverisaliar@gmail.com>
2022-04-03 22:32:51 +0700

    set view engine = ejs

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-const express = require("express");
-const req = require("express/lib/request");
+const express = require('express');
+const req = require('express/lib/request');
 const app = express();
 const path = require('path');
 require('dotenv').config();
+//set view engine cho app, ở đây sử dụng ejs(embed javascript templating, dùng để nhúng code js vào file html)(hình như bên laravel có cái gọi là Blade)
+app.set('view engine', 'ejs');
+
 
 //Express static dùng để lấy những file tĩnh mà không cần viết route
-app.use(express.static(path.join(__dirname,'..','public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-const server = app.listen(process.env.PORT || 4050,()=>{
-    console.log(`Listen complete. port ${server.address().port}`);
+const server = app.listen(process.env.PORT || 4050, () => {
+  console.log(`Listen complete. port ${server.address().port}`);
 });

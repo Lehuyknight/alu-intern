bc7715c71cfedbb38b1162ba312cfec6691a3f6e
Lê Huy Knight <loverisaliar@gmail.com>
2022-04-03 22:41:26 +0700

    add views folder path for view engine

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const path = require('path');
 require('dotenv').config();
 //set view engine cho app, ở đây sử dụng ejs(embed javascript templating, dùng để nhúng code js vào file html)(hình như bên laravel có cái gọi là Blade)
 app.set('view engine', 'ejs');
-
+//set đường dẫn cho thư mục views(view engine phải có thư mục view thì mới đọc được)
+app.set('views', path.join(__dirname, '..', 'resources', 'views'));
 
 //Express static dùng để lấy những file tĩnh mà không cần viết route
 app.use(express.static(path.join(__dirname, '..', 'public')));

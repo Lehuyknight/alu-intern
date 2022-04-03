b2732b760f8c97f6ffe9aff8b261d2d8c74416a5
Lê Huy Knight <loverisaliar@gmail.com>
2022-04-03 22:19:19 +0700

    using env file to call port

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const req = require("express/lib/request");
 const app = express();
 const path = require('path');
+require('dotenv');
 
-
-
+//Express static dùng để lấy những file tĩnh mà không cần viết route
 app.use(express.static(path.join(__dirname,'..','public')));
-app.listen(5045,()=>{
-    console.log('Listen complete');
+
+app.listen(process.env.PORT || 4050,()=>{
+    console.log(`Listen complete. port ${process.env.PORT}`);
 });

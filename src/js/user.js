f086a6abea4732c7f4ff5e27dcbdc527e902245c
Lê Huy Knight <loverisaliar@gmail.com>
2022-04-04 11:11:08 +0700

    add event for loginForm variable

--- src/js/user.js
+++ src/js/user.js
@@ -1,33 +1,35 @@
-  // IIFE - Immediately Invoked Function Expression
-  (function($, window, document) {
+// IIFE - Immediately Invoked Function Expression
+(function ($, window, document) {
+  // The $ is now locally scoped
 
-    // The $ is now locally scoped 
+  // Listen for the jQuery ready event on the document
+  $(function () {
+    // The DOM is ready!
+    const loginForm = $('#login-form');
+    loginForm.on('click', function (event) {
+      event.preventDefault();
+      const body = getFormBody($(this));
+      console.log(body);
+    });
+  });
 
-   // Listen for the jQuery ready event on the document
-   $(function() {
+  // The rest of the code goes here!
 
-     // The DOM is ready!
-      const loginForm = $('#login-form');
-   });
+  function login(body) {
+    return $.ajax({
+      url: '/login',
+      type: 'POST',
+      data: body,
+    });
+  }
 
-   // The rest of the code goes here!
-   
-   function login(body){
-       return $.ajax({
-           url:'/login',
-           type: 'POST',
-           data: body
-       })
-   }
-
-   function getFormBody(form){
-     const body = {};
-     const fields = form.serializeArray();
-     fields.forEach((field) => {
-       body[field.name] = field.value;
-     });
-     return body;
-   }
-
-  }(window.jQuery, window, document));
-  // The global jQuery object is passed as a parameter
+  function getFormBody(form) {
+    const body = {};
+    const fields = form.serializeArray();
+    fields.forEach((field) => {
+      body[field.name] = field.value;
+    });
+    return body;
+  }
+})(window.jQuery, window, document);
+// The global jQuery object is passed as a parameter
